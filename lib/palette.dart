b98107e4c38bdfb409cf8801d7f61e98015c504f
EmmanuelDaPanda <okubuikeemmanuel3@gmail.com>
2023-04-05 23:56:00 +0100

    updated colour palette

--- lib/palette.dart
+++ lib/palette.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
 class Palette {
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color lightWhite = Color(0xFFA7A7A7);
   static const Color gold = Color(0xFFCD9C3F);
+  static const Color goldAccent = Color(0xFF291F0C);
   static const Color darkBrown = Color(0xFF0F0807);
+  static const Color darkBrownAccent = Color(0xFF141212);
   static const Color lightBrown = Color(0xFF553517);
-  static const Color lightWhite = Color.fromARGB(255, 167, 167, 167);
-  static const Color white = Colors.white;
+  static const Color lightBrownAccent = Color(0xFF1D1716);
 }
